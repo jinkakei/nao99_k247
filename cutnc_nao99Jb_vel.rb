879decc63ce3ba397e0fd933c0cbf1d9487e0b85
Kei Nishina <k247loveke@gmail.com>
2016-12-13 15:00:05 +0900

    update for 16 tides

--- cutnc_nao99Jb_vel.rb
+++ cutnc_nao99Jb_vel.rb
@@ -12,8 +12,25 @@ watcher = K247_Main_Watch.new
   #tide_type = "m2" # M2: moon semi-diurnal
   #tide_type = "s2" # S2: sun semi-diurnal
   #tide_type = "k1" # K1: mixed diurnal
-  #tide_type = "o1" # O1: moon diurnal
-  tide_type = "mu2" # : diurnal
+  tide_type = "o1" # O1: moon diurnal
+
+ # main 8
+  #tide_type = "p1" # : diurnal
+  #tide_type = "n2" # : diurnal
+  #tide_type = "k2" # : diurnal
+  #tide_type = "mf" # : 14day ( not included in nao99Jb)
+  
+ # main 16
+  #tide_type = "q1" # : diurnal
+  #tide_type = "m1" # : diurnal
+  #tide_type = "j1" # : diurnal
+  #tide_type = "oo1" # : diurnal
+  #tide_type = "2n2" # : diurnal
+  #tide_type = "mu2" # : diurnal
+  #tide_type = "nu2" # : diurnal
+  #tide_type = "l2" # : diurnal
+  #tide_type = "mu2" # : diurnal
+  #tide_type = "t2" # : diurnal
 
   dname     = "nao99Jb_vel"
   nc_fn    = "#{dname}/#{tide_type}.nc"
